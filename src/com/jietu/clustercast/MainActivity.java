5ff495755d6d01814252f4373d5bfc1c309deec2
NJS98-UI <NJS98-UI@users.noreply.github.com>
2026-09-26 02:28:21 +0000

    fix: 添加 WindowManager import

--- src/com/jietu/clustercast/MainActivity.java
+++ src/com/jietu/clustercast/MainActivity.java
@@ -14,6 +14,7 @@ import android.text.method.ScrollingMovementMethod;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.AbsListView;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
